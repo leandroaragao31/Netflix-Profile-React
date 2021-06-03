9d42e0a97c98d00b21a3b514ba112c87c576c523
Leandro de Aragão Cezar <71722185+leandroaragao31@users.noreply.github.com>
2021-06-03 01:59:25 -0300

    Adicionando as imagens de perfil e alterando o estilo

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import React, { Component } from "react";
 import "./styles.css";
+import Profile1 from "./assets/profile-bolado.png";
+import Profile2 from "./assets/profile-pinguin.png";
+import Profile3 from "./assets/profile-minion.png";
+import Profile4 from "./assets/profile-smile.jpg";
+import Profile5 from "./assets/profile-robo.jpg";
 
 class Main extends Component {
   render() {
@@ -7,6 +12,13 @@ class Main extends Component {
       <main>
         <section>
           <h2>Quem está assistindo?</h2>
+          <div className="perfil-container">
+            <img src={Profile1} alt="bolado" />
+            <img src={Profile2} alt="pinguin" />
+            <img src={Profile3} alt="pinguin" />
+            <img src={Profile4} alt="pinguin" />
+            <img src={Profile5} alt="pinguin" />
+          </div>
         </section>
       </main>
     );
