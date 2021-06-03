2d66eddee612ca968042c2e3a4d1d94457958555
Leandro de Aragão Cezar <71722185+leandroaragao31@users.noreply.github.com>
2021-06-03 01:26:59 -0300

    Criando outro Component Adicionando um Component Main

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./styles.css";
+
+class Main extends Component {
+  render() {
+    return (
+      <main>
+        <section>
+          <h2>Quem está assistindo?</h2>
+        </section>
+      </main>
+    );
+  }
+}
+
+export default Main;
