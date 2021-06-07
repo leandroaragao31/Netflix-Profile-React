676374b0cebb21dd74f00c7c32d8c83c80ad251a
Leandro de Aragão Cezar <71722185+leandroaragao31@users.noreply.github.com>
2021-06-07 15:44:29 -0300

    Adicionando media adicionando media na logo para centralizar e aumentar a largura

--- src/header.js
+++ src/header.js
@@ -6,7 +6,7 @@ class Header extends Component {
   render() {
     return (
       <header>
-        <div>
+        <div className="container-logo">
           <img className="logo" src={Logo} alt="netflix-logo" />
         </div>
       </header>
